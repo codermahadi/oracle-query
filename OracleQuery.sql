db2946693b7b344a7288682094181dacb63b0c13
codermahadi <31430772+codermahadi@users.noreply.github.com>
2018-04-18 11:25:41 +0600

    Create Table Example: copying selected columns of another table

--- OracleQuery.sql
+++ OracleQuery.sql
@@ -7,3 +7,23 @@ CREATE TABLE customers
 );
 
 CREATE TABLE newCustomers AS(SELECT * FROM customers WHERE customer_id <2);
+
+CREATE TABLE regularcustomers
+(
+  IRCUSTOMER_ID NUMBER(10,0) NOT NULL ENABLE ,
+  IRCUSTOMER_NAME VARCHAR2(25) NOT NULL ENABLE ,
+  IRC_CITY VARCHAR2(50)
+);
+
+CREATE TABLE irregularcustomers
+(
+  RCUSTOMER_ID NUMBER(10,0) NOT NULL ENABLE ,
+  RCUSTOMER_NAME VARCHAR2(50) NOT NULL ENABLE ,
+  RC_CITY VARCHAR2(50)
+);
+
+CREATE TABLE customers3 AS(
+    SELECT regularcustomers.IRCUSTOMER_ID, regularcustomers.IRC_CITY,irregularcustomers.RCUSTOMER_NAME
+      FROM irregularcustomers,regularcustomers
+  WHERE regularcustomers.IRCUSTOMER_ID = irregularcustomers.RCUSTOMER_ID
+);
